9a347ff442ec9569a7e32cc3e169928e1d8a0387
Anishkumar Senthil <asenthil@login01.expanse.sdsc.edu>
2026-02-25 16:29:46 -0800

    job output for ex 3

--- Excersise 3/mklblas-double/mklblas-double/blasmm.c
+++ Excersise 3/mklblas-double/mklblas-double/blasmm.c
@@ -90,7 +90,7 @@ int test( int N) {
     // Matrix C is of size M*N, A is of size M*K, B is of size K*N
     // Col C1+ ColC2+ ... ColCN= C= A*B = A * ColB1 + A* ColB2+ ... + A*ColBN
     for (int j = 0; j < N; j++) {
-        const double *B_col = &B[j*k]; 
+        const double *B_col = &B[j*K]; 
         double *C_col = &C_dgemv[j*M]; 
 	//HERE HERE HERE
     cblas_dgemv(CblasColMajor, CblasNoTrans, M, K, 1.0, A, LDA, B_col, INCX, 0.0, C_col, INCY);
--- Excersise 3/mklblas-double/mklblas-double/run-blasmm.sh
+++ Excersise 3/mklblas-double/mklblas-double/run-blasmm.sh
@@ -7,12 +7,12 @@
 #SBATCH --partition=shared
 #Next line asks for 1 machine node and  1 core per node 
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=8
 #SBATCH --export=ALL
 # Next line limits the job execution time at most 3 minute.
 #SBATCH -t 00:03:00
 #SBATCH --account=csb175
 
-export OMP_NUM_THREADS=1
-export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=8
+export MKL_NUM_THREADS=8
 ./blasmm
